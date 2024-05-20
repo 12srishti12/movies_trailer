970a6c0b60f383846df987d3c07723ddaf414943
12srishti12 <srishtiparthsarthi@gmail.com>
2024-05-21 04:01:20 +0530

    Initial commit

---
+++ src/test/java/com/collection/movies_trailer/MoviesTrailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.movies_trailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesTrailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
